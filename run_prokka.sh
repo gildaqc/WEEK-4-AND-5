ad9448ec4534663b9b33177b2a221d725b04a8d2
gildaqc <gilda.quezadacorrea@kaust.edu.sa>
2024-10-01 16:09:42 +0300

    Update run_prokka.sh

--- run_prokka.sh
+++ run_prokka.sh
@@ -4,22 +4,18 @@
 #SBATCH--error=ash_prokka.err
 #SBATCH--time=1:00:00
 
-# Create output directory if it doesn't exist
-mkdir -p prokka_output
-
-# Clear the previous cds_counts.txt file
-> prokka_output/cds_counts.txt
+mkdir -p prokka_output # Create output directory if it doesn't exist
+> prokka_output/cds_counts.txt # Clear the previous cds_counts.txt file
 
 # Loop through all .fna files in the specified directory and subdirectories
 find /home/quezadgc/ncbi_dataset/data/ -name "GCA*.fna" | while read fna_file; do
-
     base_name=$(basename "$fna_file" .fna)
 
     # Run Prokka for each .fna file
     echo "Annotating $fna_file with Prokka..."
     prokka --outdir "prokka_output/$base_name" --prefix "$base_name" "$fna_file"
 
-    # After annotation, count CDS from the .gff file
+    # After annotation, count coding sequence (CDS) from the .gff file
     gff_file="prokka_output/$base_name/$base_name.gff"
     if [[ -f "$gff_file" ]]; then
         # Count the number of CDS in the .gff file and append to cds_counts.txt
